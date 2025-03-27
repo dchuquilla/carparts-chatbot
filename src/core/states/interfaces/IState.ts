a0948a00caec2181f23db17d4421968e7526a2d0
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-03-27 17:28:55 -0500

    ✨ Remove unused state classes and interfaces to streamline state management

--- src/core/states/interfaces/IState.ts
+++
@@ -1,7 +0,0 @@
-import { Session } from '../../session/Session';
-
-export interface IState {
-  handleInput(input: string, session: Session): Promise<Session>;
-  getNextState(session: Session): string;
-  getPrompt(session: Session): string;
-}
